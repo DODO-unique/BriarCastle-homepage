1fc3bdf9ea883bd300055e5d8762f0b33c865c40
V. Prescott <victorprescott554@gmail.com>
2026-06-29 18:09:38 +0530

    introductory component set

---
+++ src/components/hero.tsx
@@ -0,0 +1,18 @@
+import './hero.css'
+
+export default function Hero () {
+    
+    return (        
+        <div className="heading">
+            <h1 className="heading-Briar">
+                Briar
+            </h1>
+            <h1 className="heading-Castle">
+                Castle
+            </h1>
+            <p className='heading-credit'>
+                Created & Curated by <a href='https://example.com'>Gaurav M.</a>
+            </p>
+        </div>
+        )
+}
