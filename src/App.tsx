f803e6c0c93698f18587d2f11184843346ab246c
V. Prescott <victorprescott554@gmail.com>
2026-08-17 08:53:29 +0530

    chore: stash footer and header for responsive work
    Temporarily disable footer and remove site icon + label from header so main content can be refactored for responsiveness without visual interference. Footer implementation and CSS preserved inside comments for easy restoration.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import './App.css';
 import Hero from './components/hero';
 import Header from './components/header';
 import Body from './components/body';
-import Footer from './components/footer';
+// Footer intentionally not imported to keep it inactive. To re-enable, uncomment the import below.
+// import Footer from './components/footer';
 
 export default function App () {
     return (
@@ -11,7 +12,8 @@ export default function App () {
             <Hero />
             <Header />
             <Body />
-            <Footer />
+            {/* Footer intentionally not rendered. To re-enable, uncomment the line below and the import at the top of this file. */}
+            {/* <Footer /> */}
         </div>
     )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,9 @@
-import siteIcon from '../assets/android-chrome-192x192.png';
 import './header.css'
 
 export default function Header () {
     return (
         <>
             <div className="header-rect">
-                <div className='site-icon'>
-                    <img src={siteIcon} className='site-icon-img'/>
-                </div>
-                <p className='icon-scribble'>
-                    Our icon!
-                </p>
             </div>
         </>
     )
